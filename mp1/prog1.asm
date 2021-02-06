cf10432ec6613e53a5b3f6fa6392ccfb341c0352
Roy Lu <roylu2@illinois.edu>
2021-02-05 19:37:29 -0600

    MP1 Completed

--- mp1/prog1.asm
+++ mp1/prog1.asm
@@ -104,7 +104,94 @@ PRINT_HIST
 ; for your implementation, list registers used in this part of the code,
 ; and provide sufficient comments
 
+; Partners: briolat2, kaylanw4
 
+; This PRINT_HIST function is used to print the amount of every letter that
+; appears within a string. This was done by first looping through the entire
+; alphabet and have an extra slot for every other symbol (total of 27). Next,
+; you will go through each letter and convert it's binary amount into a hex,
+; which is then printed on the screen.
+	
+	; R0 - used to print and counter for 4 bits
+	; R1 - 27 loops
+	; R2 - starting ascii value
+	; R3 - current address of histogram (will be modified for bit shifting)
+	; R4 - counter for 4 hex
+	; R5 - current value of hex
+	; R6 - current address of histogram
+	; R7 - value holder of current hex
+	
+	LD R6, HIST_ADDR	; load starting address of histogram to R6
+	
+	LD R1, NUM_BINS		; 27 loops
+	
+	LD R2, ASCII_START	; starting ascii value - @
+	
+PRINT_LOOP				; main print loop
+	ADD R1, R1, #0		
+	BRz DONE			; branch to DONE if counter reaches 0
+	AND R0, R0, #0		; clear R0
+	ADD R0, R2, #0		; print current ascii value
+	OUT
+	LD R0, ASCII_SPACE
+	OUT					; print space after current ascii value
+	
+HEX_LOOP				; start of hex loop
+	AND R3, R3, #0		; clear R3
+	LDR R3, R6, #0		; load current value of histogram into R3
+	AND R4, R4, #0		; clear R4
+	ADD R4, R4, #4		; set loop to 4	(for hex counter)
+	
+	
+RESET
+	AND R0, R0, #0
+	ADD R0, R0, #4		; set loop to 4 (for bit counter)
+	AND R7, R7, #0		; clears R7
+	
+BIT_LOOP
+	ADD R0, R0, #0
+	BRz PRINT_HEX		; if bit counter reaches zero, goes to PRINT_HEX to print corresponding hex value
+	
+	ADD R7, R7, R7		; shift R7 left by one for next bit value
+	ADD R3, R3, #0		; checks if R3 is zero or positive
+	BRzp #1				; if so, skips the next step to avoid adding an additional bit
+	
+	ADD R7, R7, #1		; copies the current bit value into R7
+	
+	ADD R0, R0, #-1		; decrement counter by 1
+	ADD R3, R3, R3		; shift R4 left by one for next bit value
+	
+	BRnzp BIT_LOOP
+	
+PRINT_HEX				; prints the hex value
+
+	AND R0, R0, #0		; clear R0
+	ADD R0, R7, #-9		; put the value of hex into R0
+	BRp PRINT_HEX_TWO	; branch to PRINT_HEX_TWO if hex is greater than 9
+	LD R0, ASCII_ZERO	; load the starting value of 0 in ascii
+	ADD R0, R0, R7		; add offset value of R7 to R0
+	OUT					; print to console
+	ADD R4, R4, #-1		; decrement hex counter
+	BRp RESET			; branch to RESET if the chunk of bits were converted
+	BRnzp READY_NEXT_HEX ; branch to READY_NEXT_HEX if all bits were converted
+	
+PRINT_HEX_TWO			; prints the hex value 2
+	AND R0, R0, #0		; clear R0
+	LD R0, ASCII_A		; load the starting value of A in ascii
+	ADD R0, R0, R7
+	ADD R0, R0, #-10	; add offset value of R7 to R0
+	OUT					; print to console
+	ADD R4, R4, #-1		; decrement hex counter
+	BRp RESET			; branch to RESET if the chunk of bits were converted
+	BRnzp READY_NEXT_HEX ; branch to READY_NEXT_HEX if all bits were converted
+	
+READY_NEXT_HEX
+	ADD R2, R2, #1		; increment ascii counter
+	ADD R6, R6, #1		; increment histogram address by 1
+	ADD R1, R1, #-1		; decrement one from the 27 characters
+	LD R0, ASCII_NL		; load hex of new line
+	OUT					; print a new line
+	BRnzp PRINT_LOOP	; goes to character in histogram
 
 DONE	HALT			; done
 
@@ -114,8 +201,14 @@ NUM_BINS	.FILL #27	; 27 loop iterations
 NEG_AT		.FILL xFFC0	; the additive inverse of ASCII '@'
 AT_MIN_Z	.FILL xFFE6	; the difference between ASCII '@' and 'Z'
 AT_MIN_BQ	.FILL xFFE0	; the difference between ASCII '@' and '`'
-HIST_ADDR	.FILL x3F00     ; histogram starting address
-STR_START	.FILL x4000	; string starting address
+HIST_ADDR	.FILL x3F00 ; histogram starting address
+STR_START	.FILL x4000	; string starting add ress
+
+ASCII_START	.FILL x40   ; character used for @
+ASCII_SPACE .FILL x20	; character used for space
+ASCII_NL 	.FILL xA	; character used for new line
+ASCII_ZERO 	.FILL x30   ; the ASCII value of '0'
+ASCII_A		.FILL x41	; the ASCII value of 'A'
 
 ; for testing, you can use the lines below to include the string in this
 ; program...
